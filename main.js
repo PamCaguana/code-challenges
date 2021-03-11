5fc50bf3b214510b20e2b36e52a1a178c1662735
Pamela Caguana <pamelacaguana@gmail.com>
2021-03-11 13:58:25 -0700

    added a daily challenge

--- main.js
+++ main.js
@@ -2,3 +2,10 @@
 var replaceDots = function(str) {
     return str.replace(/[.]/g, '-');
   }
+
+//Difference of Volumes of Cuboids
+  function findDifference(a, b) {
+    let sum1 = a.reduce((a,b) => a * b)
+    let sum2 = b.reduce((a,b) => a * b)
+    return Math.abs(sum1-sum2)
+  }
