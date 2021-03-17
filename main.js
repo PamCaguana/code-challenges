fffe27bf3b45a73d3fc2c2b91bd662840f5ce234
Pamela Caguana <pamelacaguana@gmail.com>
2021-03-16 19:17:21 -0600

    added today's daily challenge

--- main.js
+++ main.js
@@ -21,3 +21,9 @@ var replaceDots = function(str) {
     let average = sum/marks.length
     return Math.floor(average)
   }
+
+  //Calculate BMI
+  function bmi(weight, height) {
+    let bmi = ((weight)/(height**2));
+    return bmi <= 18.5 ? "Underweight" : bmi <=25.0 ? "Normal" : bmi <=30.0 ? "Overweight" : "Obese"
+  }
