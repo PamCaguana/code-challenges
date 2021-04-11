3fc50822fb9c650fe75b0164b8cfeec46782d64d
Pamela Caguana <pamelacaguana@gmail.com>
2021-04-10 22:24:11 -0600

    add new code challenge

--- main.js
+++ main.js
@@ -57,3 +57,14 @@ function bonusTime(salary, bonus) {
 function makeUpperCase(str) {
   return str.toUpperCase()
 }
+
+//Thinkful - Logic Drills: Traffic light
+function updateLight(current) {
+  if (current === "green"){
+    return "yellow"
+  } else if(current === "yellow"){
+    return "red"
+  } else {
+    return "green"
+  }
+}
