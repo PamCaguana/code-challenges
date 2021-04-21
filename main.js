d2f214b3e72c4341683a13e5e2be8814c09f2adc
Pamela Caguana <pamelacaguana@gmail.com>
2021-04-21 16:02:15 -0600

    add another challenge

--- main.js
+++ main.js
@@ -219,3 +219,21 @@ function enough(cap, on, wait) {
 function enough(cap, on, wait) {
   return Math.max(wait + on - cap, 0)
 }
+
+//Drink about
+function peopleWithAgeDrink(old) {
+  if(old < 14){
+    return "drink toddy"
+  }else if(old < 18){
+    return "drink coke"
+  }else if(old < 21){
+    return "drink beer"
+  }else{
+    return "drink whisky"
+  }
+}
+
+const peopleWithAgeDrink = (age) =>
+  age < 14 ? "drink toddy" :
+  age < 18 ? "drink coke" :
+  age < 21 ? "drink beer" : "drink whisky"
