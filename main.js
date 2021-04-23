4bd39551fb1bb402d88e4f6545a17b5e4b38568c
Pamela Caguana <pamelacaguana@gmail.com>
2021-04-22 21:54:58 -0600

    add today's daily challenge

--- main.js
+++ main.js
@@ -237,3 +237,8 @@ const peopleWithAgeDrink = (age) =>
   age < 14 ? "drink toddy" :
   age < 18 ? "drink coke" :
   age < 21 ? "drink beer" : "drink whisky"
+
+  //Template Strings
+  var TempleStrings = function(obj, feature) {
+    return `${obj} are ${feature}`;
+  }
