8738f65e3e1c8e8e509f3b3aea951422ed61d60b
Pamela Caguana <pamelacaguana@gmail.com>
2021-05-01 22:26:14 -0600

    add today's daily challenge

--- main.js
+++ main.js
@@ -296,3 +296,12 @@ const peopleWithAgeDrink = (age) =>
   function numberToString(num) {
     return num.toString()
   }
+
+  //The Feast of Many Beasts
+  function feast(beast, dish) {
+    if (beast[0] === dish[0] && beast.slice(-1) === dish.slice(-1)) {
+      return true  
+    }else{
+      return false
+    }
+  }
