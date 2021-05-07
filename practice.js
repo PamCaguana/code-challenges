7e3342314c620bfe501454ad9c0e74d980239ae3
Pamela Caguana <pamelacaguana@gmail.com>
2021-05-07 11:54:07 -0600

    add practice file

---
+++ practice.js
@@ -0,0 +1,33 @@
+//Please research the following methods:
+//map
+//reduce
+//filter
+//forEach
+//sort
+//slice
+//pop
+//shift
+//push
+//unshift
+//includes
+//indexOf
+//every
+
+//Give a short description of what the method does, how it works, it's time complexity (if appropriate), and give three examples of it in action!
+
+//Questions to practice:
+
+// One:
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+// Two:
+// You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed 'age' of 0. Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
+
+// Three:
+// Sum all the numbers of the array except the highest and the lowest element (the value, not the index!).
+// Example:
+// [ 6, 2, 1, 8, 10 ] => 16
+// [ 1, 1, 11, 2, 3 ] => 6
+
+// 5 codewars tagged as arrays (minimum)
