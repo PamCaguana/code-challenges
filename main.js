178fe34d63a140856571d795bf8ee63725e37c73
Pamela Caguana <pamelacaguana@gmail.com>
2021-05-12 22:07:50 -0600

    add daily challenge

--- main.js
+++ main.js
@@ -452,3 +452,16 @@ function sumStr(a,b) {
   let sum = Number(a) + Number(b)
   return sum.toString()
 }
+
+//Opposites Attract
+function lovefunc(flower1, flower2){
+  if((flower1%2===0 && flower2%2 !==0) || (flower1%2!==0 && flower2%2===0)){
+    return true
+  }else{
+    return false
+  }
+}
+
+function lovefunc(flower1, flower2){
+  return flower1 % 2 !== flower2 % 2;
+}
