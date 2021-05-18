6bb5272b3b48eb67c0de3729c05161d1117d7451
Pamela Caguana <pamelacaguana@gmail.com>
2021-05-17 21:16:37 -0600

    add daily challenge

--- main.js
+++ main.js
@@ -577,3 +577,14 @@ var max = function(list){
     
     return Math.max(...list)
 }
+
+//Transportation on vacation
+function rentalCarCost(d) {
+  if(d >= 7){
+    return d*40 - 50
+  }else if(d >= 3){
+    return d*40 - 20
+  }else{
+    return d*40
+  }
+}
