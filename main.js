5779224aa9749f17015d3805a380093bae153c44
Pamela Caguana <pamelacaguana@gmail.com>
2021-05-18 22:20:05 -0600

    add fundamentals challenge

--- main.js
+++ main.js
@@ -625,3 +625,14 @@ function preFizz(n) {
     arr.push(i)
   }return arr
 }
+
+//What's the real floor?
+function getRealFloor(n) {
+  if(n >= 13){
+    return n - 2
+  }else if(n > 0){
+    return n -1
+  }else{
+    return n
+  }
+}
