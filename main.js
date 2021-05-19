474272b3c36833e4a32a6eba00fa8c5395182391
Pamela Caguana <pamelacaguana@gmail.com>
2021-05-18 22:16:43 -0600

    add array challenge

--- main.js
+++ main.js
@@ -596,3 +596,11 @@ function pipeFix(numbers){
     arr.push(i)
   }return arr
 }
+
+//Generate range of integers
+function generateRange(min, max, step){
+  let range = []
+  for(let i=min; i <= max; i += step){
+    range.push(i)
+  }return range
+}
