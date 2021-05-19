63eaf0576ccfd982b9f592483ea247c2fd00f0a0
Pamela Caguana <pamelacaguana@gmail.com>
2021-05-18 22:23:04 -0600

    add fundamentals + arrays challenge

--- main.js
+++ main.js
@@ -648,3 +648,12 @@ function multiTable(number) {
   }
   return table.slice(0, -1)
 }
+
+//Find the first non-consecutive number
+function firstNonConsecutive (arr) {
+  for(let i=0; i<arr.length-1;i++){
+    if(arr[i+1] !== arr[i]+1){
+      return arr[i+1]
+    }
+  }return null
+}
