535a6401c2cc03a5e3744cfd719e6561587396c2
Pamela Caguana <pamelacaguana@gmail.com>
2021-05-19 21:58:33 -0600

    add daily challenge

--- main.js
+++ main.js
@@ -657,3 +657,13 @@ function firstNonConsecutive (arr) {
     }
   }return null
 }
+
+//101 Dalmatians - squash the bugs, not the dogs!
+function howManyDalmatians(number){
+  
+  let dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"]
+  
+  let respond = number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number == 101 ? dogs[3] : dogs[2]
+  
+  return respond
+}
