91dcd56803c5d5b5f2ae3b6430745f5219d147da
Pamela Caguana <pamelacaguana@gmail.com>
2021-05-23 22:56:07 -0600

    add daily challenge

--- main.js
+++ main.js
@@ -711,3 +711,13 @@ const reverseSeq = n => {
   function solution(str){
     return str.split('').reverse().join('')
   }
+
+  //DNA to RNA Conversion
+  function DNAtoRNA(dna) {
+    let arr = dna.split('')
+    for(let i=0; i<arr.length;i++){
+      if(arr[i]==="T"){
+        arr[i] = "U"
+      }
+    }return arr.join('')
+  }
