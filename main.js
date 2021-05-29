a1c6c9e881e22a054f7fe87f5937709717e4d083
Pamela Caguana <pamelacaguana@gmail.com>
2021-05-29 15:37:45 -0600

    add 7kyu challenge

--- main.js
+++ main.js
@@ -787,3 +787,17 @@ const reverseSeq = n => {
     return -1;
     }
   }
+
+  //Alphabet symmetry
+  function solve(arr){  
+    return arr.map(function(input){
+      let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+      let arr = input.toLowerCase().split('')
+      let sum = 0
+      arr.forEach(function(val, i) {
+          if(val === alphabet[i])
+                  sum++
+       })
+      return sum
+    })
+  }
