ae6c9b07d1ad89e141d1a6200b9a13aa26d3ea20
Pamela Caguana <pamelacaguana@gmail.com>
2021-06-29 13:51:48 -0400

    add Bug Fixing 1

--- main.js
+++ main.js
@@ -1288,3 +1288,12 @@ const rps = (p1, p2) => {
     return 'Player 2 won!'
   }
 }
+
+//Unfinished Loop - Bug Fixing #1
+function createArray(number){
+  var newArray = []
+  for(var counter = 1; counter <= number; counter++){
+    newArray.push(counter)
+  }  
+  return newArray
+}
