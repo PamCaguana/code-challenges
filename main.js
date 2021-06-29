ac9b781e671fa72d0afaf6fccb3a84e5f89657e2
Pamela Caguana <pamelacaguana@gmail.com>
2021-06-29 13:51:00 -0400

    add Rock Paper Scissors

--- main.js
+++ main.js
@@ -1277,3 +1277,14 @@ function betterThanAverage(classPoints, yourPoints) {
 function areYouPlayingBanjo(name) {
   return name[0].toLowerCase() === 'r' ? `${name} plays banjo` : `${name} does not play banjo`
 }
+
+//Rock Paper Scissors!
+const rps = (p1, p2) => {
+  if((p1 == 'scissors' && p2 === 'paper') || (p1 == 'paper' && p2 === 'rock') || (p1 == 'rock' && p2 === 'scissors')){
+    return 'Player 1 won!'
+  }else if(p1 == p2){
+    return 'Draw!'
+  }else{
+    return 'Player 2 won!'
+  }
+}
