55074ecae94071303e0b575d1b828f63b744383c
Pamela Caguana <pamelacaguana@gmail.com>
2021-07-06 23:32:39 -0400

    add Basic Mathematical Operations

--- main.js
+++ main.js
@@ -1431,3 +1431,30 @@ function convertToCelsius (temperature) {
 function reverseList(list) {
   return list.reverse()
 }
+
+//Basic Mathematical Operations
+function basicOp(operation, value1, value2){
+  if(operation === '+')
+    return value1 + value2
+  else if(operation === '-')
+    return value1 - value2
+  else if(operation === '*')
+    return value1 * value2
+  else
+    return value1 / value2
+}
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+      case '+':
+          return value1 + value2;
+      case '-':
+          return value1 - value2;
+      case '*':
+          return value1 * value2;
+      case '/':
+          return value1 / value2;
+      default:
+          return 0;
+  }
+}
