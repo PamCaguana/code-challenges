23ecc9f1f86ec4dc289fbd5124530d4478f3d114
Pamela Caguana <pamelacaguana@gmail.com>
2021-07-12 19:10:55 -0400

    add Switch it Up

--- main.js
+++ main.js
@@ -1539,3 +1539,29 @@ function doubleChar(str) {
 
   const hello = s =>
   `Hello, ${s ? (s[0].toUpperCase() + s.slice(1).toLowerCase()) : 'World'}!`
+
+  //Switch it Up!
+  function switchItUp(number){
+    switch (number) {
+     case 0 : 
+       return 'Zero'
+     case 1 : 
+       return 'One'
+     case 2 : 
+       return 'Two'
+     case 3 : 
+       return 'Three'
+     case 4 : 
+       return 'Four'
+     case 5 : 
+       return 'Five'
+     case 6 : 
+       return 'Six'
+     case 7 : 
+       return 'Seven'
+     case 8 : 
+       return 'Eight'
+     case 9 : 
+       return 'Nine'
+   }
+ }
