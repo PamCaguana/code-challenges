4532ea744fb38dd41e97f37bf9cc16f7b3e1f7bd
Pamela Caguana <pamelacaguana@gmail.com>
2021-07-23 19:17:36 -0600

    add Friend or Foe

--- main.js
+++ main.js
@@ -1700,3 +1700,17 @@ function calculateTip(amount, rating) {
     default:return "Rating not recognised";
   }
 }
+
+//Friend or Foe?
+function friend(friends){
+  let arr = []
+  for(let i=0;i < friends.length; i++){
+    if(friends[i].length === 4){
+      arr.push(friends[i])
+    }
+  }return arr
+}
+
+function friend(friends){
+  return friends.filter(n => n.length === 4)
+}
