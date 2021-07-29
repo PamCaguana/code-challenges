557a4a6a43614672897bf9d2312b3fd64bf71300
Pamela Caguana <pamelacaguana@gmail.com>
2021-07-28 22:28:34 -0600

    add Find the Divisors

--- main.js
+++ main.js
@@ -1762,3 +1762,13 @@ var number = function(busStops){
 
 const number = busStops => busStops.reduce((p,n) => p+n[0]-n[1],0)
 const number = busStops => busStops.reduce((rem, [on, off]) => rem + on - off, 0)
+
+//Find the divisors! 
+function divisors(integer) {
+  let arr = []
+  for(let i=2; i < integer; i++){
+    if(integer % i === 0){
+      arr.push(i)
+    }
+  }return arr.length > 1 ? arr : `${integer} is prime`
+}
