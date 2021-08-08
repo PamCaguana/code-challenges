9248e5342bb0732f0185112b3a2056316968fe17
Pamela Caguana <pamelacaguana@gmail.com>
2021-08-07 22:43:07 -0600

    add Stop Spinning My Words

--- main.js
+++ main.js
@@ -1891,3 +1891,13 @@ function nameShuffler(str){
 function twoDecimalPlaces(n) {
   return +n.toFixed(2)
 }
+
+//Stop gninnipS My sdroW!
+function spinWords(string){
+  return string.replace(/\w{5,}/g, function(w){return w.split('').reverse().join('')})
+}
+function spinWords(words){
+  return words.split(' ').map(function (word) {
+    return (word.length > 4) ? word.split('').reverse().join('') : word
+  }).join(' ')
+}
